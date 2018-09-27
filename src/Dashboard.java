277672d7a02735e5c81a6b1019c1f7e4b3d2ec28
Chris <kaloshgimy@gmail.com>
2018-09-27 22:03:58 +0300

    Add user page successfully saving users to database

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -1,5 +1,10 @@
 
 import java.lang.reflect.Constructor;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JFrame;
 
 /*
@@ -12,13 +17,31 @@ import javax.swing.JFrame;
  * @author khanflex
  */
 public class Dashboard extends javax.swing.JFrame {
-
+    Connection connection;
     /**
      * Creates new form Dashboard
      */
     public Dashboard() {
         initComponents();
         lbl_userString.setText("WELCOME, " + User.username.toUpperCase());
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/frontdeskapplication", "chris", "password");
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery("select * from normalusers");
+
+            while (results.next()) {
+                System.out.println("id : " + results.getString(1));
+                System.out.println("fname : " + results.getString(2));
+                System.out.println("lname : " + results.getString(3));
+                System.out.println("lname : " + results.getString(4));
+                System.out.println("lname : " + results.getString(5));
+                System.out.println("lname : " + results.getString(6));
+            }
+
+            System.out.println("Try block success");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     /**
@@ -44,13 +67,10 @@ public class Dashboard extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Name", "Gender", "Telephone", "Date of Birth", "Age"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
